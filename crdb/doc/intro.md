9ed95ad62b1868d6d8b9d5b82e69d29f711937d8
Ranadeep Biswas <ranadip.bswas@gmail.com>
2021-03-12 16:56:23 +0100

    new cockroachdb tests

---
+++ crdb/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to crdb
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ crdb/test/crdb/core_test.clj
@@ -0,0 +1,7 @@
+(ns crdb.core-test
+  (:require [clojure.test :refer :all]
+            [crdb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
