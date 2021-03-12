341f4473abdfe22640ebaa90b1566e88b5c221b3
Ranadeep Biswas <ranadip.bswas@gmail.com>
2021-03-12 10:03:39 +0100

    rnb - jepsen setup

--- cockroachdb/project.clj
+++ cockroachdb/project.clj
@@ -3,17 +3,13 @@
   :url "http://cockroachlabs.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [jepsen "0.1.9-SNAPSHOT"]
-                 [org.clojure/java.jdbc "0.6.1"]
-                 [circleci/clj-yaml "0.5.5"]
-                 [org.postgresql/postgresql "9.4.1211"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [jepsen "0.2.4-SNAPSHOT"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [circleci/clj-yaml "0.6.0"]
+                 [org.postgresql/postgresql "42.2.19"]]
   :jvm-opts ["-Xmx12g"
-             "-XX:+UseConcMarkSweepGC"
-             "-XX:+UseParNewGC"
              "-XX:+CMSParallelRemarkEnabled"
-             "-XX:+AggressiveOpts"
-             "-XX:+UseFastAccessorMethods"
              "-XX:MaxInlineLevel=32"
              "-XX:MaxRecursiveInlineLevel=2"
              "-server"]
